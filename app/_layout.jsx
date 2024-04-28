5cb547efc223be5711cad3a8467e87fdb7c095b9
rchellee <ruerasritchelle07@gmail.com>
2024-04-28 10:22:48 +0800

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
-import { useEffect } from 'react';
+import { useEffect } from "react";
+
+import GlobalProvider from "../context/GlobalProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -20,7 +22,6 @@ const RootLayout = () => {
 
   useEffect(() => {
     if (error) throw error;
-
     if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
@@ -30,9 +31,14 @@ const RootLayout = () => {
     return null;
   }
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+      </Stack>
+    </GlobalProvider>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -5,24 +5,32 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
-          <Image source={images.logo} className="w-[130px] h-[84px]" />
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[200px] h-[84px]"
+            resizeMode="contain"
+          />
 
           <Image
             source={images.cards}
             className="max-w-[380px] w-full h-[380px]"
-            resizeMode="contain"
+            // resizeMode="contain"
           />
 
-          <View className="relative mt-5">
+          <View className="relative mt-1">
             <Text className="text-2xl text-white font-bold text-center">
-              Discover Endless{"\n"}
-              Possibilities with{" "}
+              You can make a{"\n"}
+              Difference with{" "}
               <Text className="text-secondary-200">WanderPets</Text>
             </Text>
 
@@ -34,13 +42,12 @@ export default function App() {
           </View>
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-            Where Creativity Meets Innovation: Embark on a Journey of Limitless
-            Exploration with WanderPets
+            Find you new best friend. Every fur deserve a FurEver Home
           </Text>
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => router.push('/sign-in')}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
