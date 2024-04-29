27ca81ecbfa3c424204c8be7b0f6c7fd6f2852fc
Monique <cabigtingmoniquekyle@gmail.com>
2024-04-30 00:23:14 +0800

    pet education
    incomplete, di ko gets yung code pa huhu

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -55,7 +55,7 @@ const SignIn = () => {
           />
 
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
-            Log in to Aora
+            Log in to Wanderpets
           </Text>
 
           <FormField
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FlatList, Image, RefreshControl, Text, View } from "react-native";
-
 import { images } from "../../constants";
 import useAppwrite from "../../lib/useAppwrite";
 import { getAllPosts } from "../../lib/appwrite";
 import EmptyState  from "../../components/EmptyState";
 import SearchInput from "../../components/SearchInput";
-import VideoCard from "../../components/VideoCard"
+import VideoCard from "../../components/VideoCard";
+import { Video } from "expo-av";
 
 const Home = () => {
   return (
-    <SafeAreaView className="bg-primary h-full">
+    <SafeAreaView className="bg-white h-full">
       <FlatList
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
@@ -22,17 +22,32 @@ const Home = () => {
                   className="w-[46px] h-[46px] rounded-lg mr-2"
                   resizeMode="cover"
                 />
-                <Text className="text-xl font-psemibold text-white">
+                <Text className="text-xl font-psemibold text">
                   Ritchelle Rueras
                 </Text>
               </View>
             </View>
-            
             <View className="w-full flex-1 pt-5 pb-8">
-              <Text className="text-gray-100 text-lg font-pregular mb-3">
-                Pet Education!
+              <Text className="text-black-100 text-lg font-pregular mb-3">
+                Pet Education
               </Text>
-            </View>
+              <Text className="text-black-100 text-lg font-pregular mb-3">
+                Welcome! Here you'll find informative videos to help you become a better pet owner.
+              </Text>
+              <View style={{ width: 300, height: 200 }}>
+                <Video
+                  source={require("../../assets/videos/educ1.mp4")}
+                  style={{ flex: 1 }}
+                  useNativeControls
+                /> 
+                <Video
+                  source={require("../../assets/videos/educ2.mp4")}
+                  style={{ flex: 1 }}
+                  useNativeControls
+                />
+        
+              </View>
+            </View> 
           </View>
         )}
       />
